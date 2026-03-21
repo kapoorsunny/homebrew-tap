b67e75ffd91a6371bce829bdfd71620b1b6d0c15
Abhishek Kapoor <kapoor@capecode.in>
2026-03-21 16:12:01 +0000

    Update mcpw v1.0.0 SHA256 hashes for rebuilt release

--- Formula/mcpw.rb
+++ Formula/mcpw.rb
@@ -7,24 +7,24 @@ class Mcpw < Formula
   on_macos do
     on_arm do
       url "https://github.com/kapoorsunny/AnythingMCP/releases/download/v#{version}/mcpw-aarch64-apple-darwin.tar.gz"
-      sha256 "043b31b7de22b07420b20f3ef89211d352b17822e6de9ed0b35fe2879de12758"
+      sha256 "04d740455c5242679fd52490064a5d8ab269f6c96a7135767f4e6b948bfc14ea"
     end
 
     on_intel do
       url "https://github.com/kapoorsunny/AnythingMCP/releases/download/v#{version}/mcpw-x86_64-apple-darwin.tar.gz"
-      sha256 "fc7a8d21ae6a2f03faa4e1e479b9e155dcba961f2244239574e567d4011238bb"
+      sha256 "68dfd1a0337da39a5420946308e971705f5d1582b9f398bd874f3cb27f845746"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/kapoorsunny/AnythingMCP/releases/download/v#{version}/mcpw-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "5256dec4cb562f7d47f315f2a9e52c612849b0164aad1bd4e49e2216fa063bc3"
+      sha256 "af9a48e3336f67b7f10481f5c5285ba8ddf7872acc4576221504deee2b05d363"
     end
 
     on_intel do
       url "https://github.com/kapoorsunny/AnythingMCP/releases/download/v#{version}/mcpw-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e257c81812121d0954f6099e9c71ed617884dcfa011b55cc52729b5bae88a9a2"
+      sha256 "a61cd81a6a690a569ad5a8090394ba8c756f63d963651bf75395c6bf5f5d5a75"
     end
   end
 
